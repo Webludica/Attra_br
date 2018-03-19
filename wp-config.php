12cdde866fb0122f9ff7c18ee69c9f4e2987dbda
Thiago <thiagoabreu@webludica.com.br>
2018-03-19 11:24:16 -0300

    host

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'thiagoabreu_attra_br_principal');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'thiagoabreu_webludic_admin_2');
+define('DB_USER', 'thiagoabreu_webludic_admin');
 
 /** Senha do banco de dados MySQL */
 define('DB_PASSWORD', 'webtab2017');
