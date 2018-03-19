7a4f969d594013ba0c8693f03ac39ca50b589383
Thiago <thiagoabreu@webludica.com.br>
2018-03-19 12:18:01 -0300

    servidor externo

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'attra_br');
+define('DB_NAME', 'thiagoabreu_attra_br_principal');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'root');
+define('DB_USER', 'thiagoabreu_attra_br_admin');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'attrabruser@');
 
 /** Nome do host do MySQL */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '149.56.247.112');
 
 /** Charset do banco de dados a ser usado na criação das tabelas. */
 define('DB_CHARSET', 'utf8mb4');
